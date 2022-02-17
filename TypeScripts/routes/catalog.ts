0512ec25b1009b8ef7af7fdcb4a007fe58db31a1
anthony <anthonydwan@outlook.com>
2022-02-17 13:36:24 +1100

    currently stuck with routing and pages not displaying

--- TypeScripts/routes/catalog.ts
+++ TypeScripts/routes/catalog.ts
@@ -3,9 +3,9 @@ var router = express.Router();
 
 // Require controller modules.
 var animal_controller = require('../controllers/animalController');
-var animal_instance_controller = require('../controllers/animalinstanceController');
-var attribute_controller = require('../controllers/attributesController');
-var bioorder_controller = require('../controllers/bioorderController');
+// var animal_instance_controller = require('../controllers/animalinstanceController');
+// var attribute_controller = require('../controllers/attributesController');
+// var bioorder_controller = require('../controllers/bioorderController');
 
 /// BOOK ROUTES ///
 
@@ -15,91 +15,91 @@ router.get('/', animal_controller.index);
 // GET request for list of all animal species.
 router.get('/animals', animal_controller.animal_list);
 
-/// bioorder ROUTES ///
+// /// bioorder ROUTES ///
 
-// GET request for one bioorder.
-router.get('/category/:id', bioorder_controller.bioorder_detail);
+// // GET request for one bioorder.
+// router.get('/category/:id', bioorder_controller.bioorder_detail);
 
-// GET request for list of all bioorders.
-router.get('/categoriess', bioorder_controller.bioorder_list);
+// // GET request for list of all bioorders.
+// router.get('/categories', bioorder_controller.bioorder_list);
 
-/// attributes ROUTES ///
+// /// attribute ROUTES ///
 
-// GET request for creating a attributes. NOTE This must come before route that displays attributes (uses id).
-router.get('/attributes/create', attribute_controller.attribute_create_get);
+// // GET request for creating a attribute. NOTE This must come before route that displays attribute (uses id).
+// router.get('/attribute/create', attribute_controller.attribute_create_get);
 
-//POST request for creating attributes.
-router.post('/attributes/create', attribute_controller.attribute_create_post);
+// //POST request for creating attribute.
+// router.post('/attribute/create', attribute_controller.attribute_create_post);
 
-// GET request to delete attributes.
-router.get('/attributes/:id/delete', attribute_controller.attribute_delete_get);
+// // GET request to delete attribute.
+// router.get('/attribute/:id/delete', attribute_controller.attribute_delete_get);
 
-// POST request to delete attributes.
-router.post(
-  '/attributes/:id/delete',
-  attribute_controller.attribute_delete_post
-);
+// // POST request to delete attribute.
+// router.post(
+//   '/attribute/:id/delete',
+//   attribute_controller.attribute_delete_post
+// );
 
-// GET request to update attributes.
-router.get('/attributes/:id/update', attribute_controller.attribute_update_get);
+// // GET request to update attribute.
+// router.get('/attribute/:id/update', attribute_controller.attribute_update_get);
 
-// POST request to update attributes.
-router.post(
-  '/attributes/:id/update',
-  attribute_controller.attribute_update_post
-);
+// // POST request to update attribute.
+// router.post(
+//   '/attribute/:id/update',
+//   attribute_controller.attribute_update_post
+// );
 
-// GET request for one attributes.
-router.get('/attributes/:id', attribute_controller.attribute_detail);
+// // GET request for one attribute.
+// router.get('/attribute/:id', attribute_controller.attribute_detail);
 
-// GET request for list of all attributes.
-router.get('/attributess', attribute_controller.attribute_list);
+// // GET request for list of all attribute.
+// router.get('/attributes', attribute_controller.attribute_list);
 
-/// animalinstance ROUTES ///
+// /// animalinstance ROUTES ///
 
-// GET request for creating a animalinstance. NOTE This must come before route that displays animalinstance (uses id).
-router.get(
-  '/animalinstance/create',
-  animal_instance_controller.animalinstance_create_get
-);
+// // GET request for creating a animalinstance. NOTE This must come before route that displays animalinstance (uses id).
+// router.get(
+//   '/animalinstance/create',
+//   animal_instance_controller.animalinstance_create_get
+// );
 
-// POST request for creating animalinstance.
-router.post(
-  '/animalinstance/create',
-  animal_instance_controller.animalinstance_create_post
-);
+// // POST request for creating animalinstance.
+// router.post(
+//   '/animalinstance/create',
+//   animal_instance_controller.animalinstance_create_post
+// );
 
-// GET request to delete animalinstance.
-router.get(
-  '/animalinstance/:id/delete',
-  animal_instance_controller.animalinstance_delete_get
-);
+// // GET request to delete animalinstance.
+// router.get(
+//   '/animalinstance/:id/delete',
+//   animal_instance_controller.animalinstance_delete_get
+// );
 
-// POST request to delete animalinstance.
-router.post(
-  '/animalinstance/:id/delete',
-  animal_instance_controller.animalinstance_delete_post
-);
+// // POST request to delete animalinstance.
+// router.post(
+//   '/animalinstance/:id/delete',
+//   animal_instance_controller.animalinstance_delete_post
+// );
 
-// GET request to update animalinstance.
-router.get(
-  '/animalinstance/:id/update',
-  animal_instance_controller.animalinstance_update_get
-);
+// // GET request to update animalinstance.
+// router.get(
+//   '/animalinstance/:id/update',
+//   animal_instance_controller.animalinstance_update_get
+// );
 
-// POST request to update animalinstance.
-router.post(
-  '/animalinstance/:id/update',
-  animal_instance_controller.animalinstance_update_post
-);
+// // POST request to update animalinstance.
+// router.post(
+//   '/animalinstance/:id/update',
+//   animal_instance_controller.animalinstance_update_post
+// );
 
-// GET request for one animalinstance.
-router.get(
-  '/animalinstance/:id',
-  animal_instance_controller.animalinstance_detail
-);
+// // GET request for one animalinstance.
+// router.get(
+//   '/animalinstance/:id',
+//   animal_instance_controller.animalinstance_detail
+// );
 
-// GET request for list of all animalinstance.
-router.get('/animalinstances', animal_instance_controller.animalinstance_list);
+// // GET request for list of all animalinstance.
+// router.get('/animalinstances', animal_instance_controller.animalinstance_list);
 
 module.exports = router;
